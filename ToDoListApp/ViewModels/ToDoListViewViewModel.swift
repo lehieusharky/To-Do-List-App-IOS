3ee637bcbae4b519f75249e9ed3d78bda2e0d1d3
lehieusharky <lehieultt@gmail.com>
2024-02-09 21:16:54 +0700

    Create new todo item view

---
+++ ToDoListApp/ViewModels/ToDoListViewViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoListViewViewModel.swift
+//  ToDoListApp
+//
+//  Created by Le Hieu on 09/02/2024.
+//
+
+import Foundation
+
+class ToDoListViewViewModel : ObservableObject {
+    @Published var showingCreateToDoItem  = false
+    
+    init() {
+        
+    }
+}
